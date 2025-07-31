7609437f4525e58a850e6c60995e3a9c99fba51e
Joshua <70722512+joshua-sx@users.noreply.github.com>
2025-07-31 14:35:50 -0400

    Rename sonner toaster alias

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { ProfilePage } from "@/components/profile/ProfilePage";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -61,7 +61,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
+      <SonnerToaster />
       <BrowserRouter>
         <DemoModeProvider>
           <AuthDebugPanel />
